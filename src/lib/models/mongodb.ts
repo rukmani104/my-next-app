3ff32079da9bc0e324ac950ef9d1e68d5e6da855
Abhishek Patel <abhishekp@theargusconsulting.com>
2025-09-26 12:53:18 +0530

    latest changes

--- src/lib/models/mongodb.ts
+++ src/lib/models/mongodb.ts
@@ -36,4 +36,5 @@ if (process.env.NODE_ENV === "development") {
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
+//kjiud
 export default clientPromise;
